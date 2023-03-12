31340775493861f260437fa7504047f0749ce144
Brandon Herrera <brandondh769@gmail.com>
2023-03-12 16:39:19 -0600

    Add main menu screen and resources

---
+++ src/main/java/Cifrador/Start.java
@@ -0,0 +1,246 @@
+package Cifrador;
+
+import javax.swing.ImageIcon;
+
+public class Start extends javax.swing.JFrame {
+
+    int x, y; 
+    static ImageIcon icon;
+    
+    public Start() {
+        initComponents();
+        close_btn.setIcon(new ImageIcon("src/main/java/icons/close_disabled.png")); 
+        title.setIcon(new ImageIcon("src/main/java/icons/favicon.png"));
+        btnSelectText.setIcon(new ImageIcon("src/main/java/icons/text_icon.png"));
+        btnSelectImage.setIcon(new ImageIcon("src/main/java/icons/image_icon.png"));
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        close_btn = new javax.swing.JButton();
+        title = new javax.swing.JLabel();
+        btnSelectText = new javax.swing.JButton();
+        btnSelectImage = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(102, 102, 102), 1, true));
+        jPanel1.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                jPanel1MouseDragged(evt);
+            }
+        });
+        jPanel1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jPanel1MousePressed(evt);
+            }
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                jPanel1MouseReleased(evt);
+            }
+        });
+
+        close_btn.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        close_btn.setBorderPainted(false);
+        close_btn.setContentAreaFilled(false);
+        close_btn.setFocusable(false);
+        close_btn.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                close_btnMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                close_btnMouseExited(evt);
+            }
+        });
+        close_btn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                close_btnActionPerformed(evt);
+            }
+        });
+
+        title.setFont(new java.awt.Font("Leelawadee UI", 1, 24)); // NOI18N
+        title.setText("Cifrador");
+
+        btnSelectText.setFont(new java.awt.Font("Yu Gothic UI", 0, 18)); // NOI18N
+        btnSelectText.setText("Texto");
+        btnSelectText.setBorder(null);
+        btnSelectText.setContentAreaFilled(false);
+        btnSelectText.setFocusPainted(false);
+        btnSelectText.setFocusable(false);
+        btnSelectText.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btnSelectTextMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnSelectTextMouseExited(evt);
+            }
+        });
+        btnSelectText.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSelectTextActionPerformed(evt);
+            }
+        });
+
+        btnSelectImage.setFont(new java.awt.Font("Yu Gothic UI", 0, 18)); // NOI18N
+        btnSelectImage.setText("Imagen");
+        btnSelectImage.setBorder(null);
+        btnSelectImage.setContentAreaFilled(false);
+        btnSelectImage.setFocusPainted(false);
+        btnSelectImage.setFocusable(false);
+        btnSelectImage.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btnSelectImageMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnSelectImageMouseExited(evt);
+            }
+        });
+        btnSelectImage.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSelectImageActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(39, 39, 39)
+                .addComponent(title)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(close_btn, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(22, 22, 22))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(74, 74, 74)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnSelectImage, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnSelectText, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(82, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(title)
+                    .addComponent(close_btn, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 72, Short.MAX_VALUE)
+                .addComponent(btnSelectText, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(31, 31, 31)
+                .addComponent(btnSelectImage, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(169, 169, 169))
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void close_btnMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_close_btnMouseEntered
+        close_btn.setIcon(new ImageIcon("src/main/java/icons/close_enabled.png"));
+    }//GEN-LAST:event_close_btnMouseEntered
+
+    private void close_btnMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_close_btnMouseExited
+        close_btn.setIcon(new ImageIcon("src/main/java/icons/close_disabled.png"));
+    }//GEN-LAST:event_close_btnMouseExited
+
+    private void close_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_close_btnActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_close_btnActionPerformed
+
+    private void jPanel1MouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel1MouseDragged
+        this.setLocation(this.getLocation().x + evt.getX()-x, this.getLocation().y + evt.getY()-y );
+        this.setOpacity(0.9f);
+    }//GEN-LAST:event_jPanel1MouseDragged
+
+    private void jPanel1MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel1MousePressed
+        x = evt.getX();
+        y = evt.getY();
+    }//GEN-LAST:event_jPanel1MousePressed
+
+    private void jPanel1MouseReleased(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel1MouseReleased
+        this.setOpacity(1f);
+    }//GEN-LAST:event_jPanel1MouseReleased
+
+    private void btnSelectTextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSelectTextActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnSelectTextActionPerformed
+
+    private void btnSelectTextMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSelectTextMouseEntered
+        btnSelectText.setForeground(new java.awt.Color(51, 51, 51));
+        btnSelectText.setBorder(javax.swing.BorderFactory.createLineBorder(java.awt.Color.BLACK, 1));
+    }//GEN-LAST:event_btnSelectTextMouseEntered
+
+    private void btnSelectTextMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSelectTextMouseExited
+        btnSelectText.setForeground(java.awt.Color.BLACK);
+        btnSelectText.setBorder(null);
+    }//GEN-LAST:event_btnSelectTextMouseExited
+
+    private void btnSelectImageMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSelectImageMouseEntered
+        btnSelectImage.setForeground(new java.awt.Color(51, 51, 51));
+        btnSelectImage.setBorder(javax.swing.BorderFactory.createLineBorder(java.awt.Color.BLACK, 1));
+    }//GEN-LAST:event_btnSelectImageMouseEntered
+
+    private void btnSelectImageMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSelectImageMouseExited
+        btnSelectImage.setForeground(java.awt.Color.BLACK);
+        btnSelectImage.setBorder(null);
+    }//GEN-LAST:event_btnSelectImageMouseExited
+
+    private void btnSelectImageActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSelectImageActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnSelectImageActionPerformed
+
+    public static void main(String args[]) {
+
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Windows".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Start.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Start.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Start.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Start.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Start().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnSelectImage;
+    private javax.swing.JButton btnSelectText;
+    private javax.swing.JButton close_btn;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel title;
+    // End of variables declaration//GEN-END:variables
+}
--- src/main/java/com/escom/ipn/cifrador/Cifrador.java
+++ src/main/java/com/escom/ipn/cifrador/Cifrador.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.escom.ipn.cifrador;
 
-/**
- *
- * @author Brand
- */
 public class Cifrador {
 
     public static void main(String[] args) {
